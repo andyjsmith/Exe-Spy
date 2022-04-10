d78014736ff371ef54cb1aee67822e232efe37e2
Andy Smith <andy@ajsmith.us>
2022-04-10 01:54:15 -0400

    Added imports tab

--- exespy/views/imports.py
+++ exespy/views/imports.py
@@ -1,11 +1,39 @@
 import PySide6.QtWidgets as QtWidgets
 
 from .. import pe_file
+from .components import table
 
 
-class ImportsView(QtWidgets.QWidget):
+class ImportsView(QtWidgets.QScrollArea):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
+        # Set up scroll area
+        self.setWidgetResizable(True)
+        self.scroll_area = QtWidgets.QWidget(self)
+        self.setWidget(self.scroll_area)
+        self.scroll_area.setLayout(QtWidgets.QFormLayout())
+
+        # Libraries
+        self.imports_group = table.TableGroup(
+            "Imports", fit_columns=True, headers=["Name", "Library"])
+        self.scroll_area.layout().addWidget(self.imports_group)
+
     def load(self, pe_obj: pe_file.PEFile):
-        ...
+        # Libraries
+        imports_list = []
+        for import_obj in pe_obj.pe.DIRECTORY_ENTRY_IMPORT:
+            for import_func in import_obj.imports:
+                if import_func.name:
+                    imports_list.append((
+                        import_func.name.decode("utf-8").strip('\x00'),
+                        import_obj.dll.decode("utf-8").strip('\x00'),
+                    ))
+                else:
+                    imports_list.append((
+                        f"[Ordinal {import_func.ordinal}]",
+                        import_obj.dll.decode("utf-8").strip('\x00'),
+                    ))
+
+        self.imports_group.view.setModel(table.TableModel(
+            imports_list, headers=["Name", "Library"]))
--- exespy/views/libraries.py
+++ exespy/views/libraries.py
@@ -22,7 +22,7 @@ class LibrariesView(QtWidgets.QScrollArea):
     def load(self, pe_obj: pe_file.PEFile):
         # Libraries
         libraries_list = []
-        for i, import_obj in enumerate(pe_obj.pe.DIRECTORY_ENTRY_IMPORT):
+        for import_obj in pe_obj.pe.DIRECTORY_ENTRY_IMPORT:
             libraries_list.append((
                 import_obj.dll.decode("utf-8").strip('\x00'),
                 len(import_obj.imports),
