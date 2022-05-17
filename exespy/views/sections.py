7f7761d9c0fbdc9339f17139e998023980d1f6d7
Andy Smith <andy@ajsmith.us>
2022-05-17 02:51:15 -0400

    Updated section to include all section table props

--- exespy/views/sections.py
+++ exespy/views/sections.py
@@ -12,19 +12,25 @@ class SectionsView(QtWidgets.QWidget):
 
         self.setLayout(QtWidgets.QVBoxLayout())
 
+        self.headers = [
+            "Name",
+            "VirtualSize",
+            "VirtualAddress",
+            "SizeOfRawData",
+            "PointerToRawData",
+            "PointerToRelocations",
+            "PointerToLinenumbers",
+            "NumberOfRelocations",
+            "NumberOfLinenumbers",
+            "Characteristics",
+        ]
+
         # Sections
         self.sections_table = table.TableView(
             fit_columns=True,
             fit_to_contents=False,
             expand_last_column=True,
-            headers=[
-                "Name",
-                "VirtualSize",
-                "VirtualAddress",
-                "SizeOfRawData",
-                "PointerToRawData",
-                "Characteristics",
-            ],
+            headers=self.headers,
         )
 
         self.layout().addWidget(self.sections_table)
@@ -41,6 +47,10 @@ class SectionsView(QtWidgets.QWidget):
                     hex(section.VirtualAddress),
                     section.SizeOfRawData,
                     hex(section.PointerToRawData),
+                    hex(section.PointerToRelocations),
+                    hex(section.PointerToLinenumbers),
+                    section.NumberOfRelocations,
+                    section.NumberOfLinenumbers,
                     f"{hex(section.Characteristics)} ({pe_obj.section_characteristics_str(i)})",
                 )
             )
@@ -48,14 +58,7 @@ class SectionsView(QtWidgets.QWidget):
         self.sections_table.setModel(
             table.TableModel(
                 sections_list,
-                headers=[
-                    "Name",
-                    "VirtualSize",
-                    "VirtualAddress",
-                    "SizeOfRawData",
-                    "PointerToRawData",
-                    "Characteristics",
-                ],
+                headers=self.headers,
             )
         )
 
